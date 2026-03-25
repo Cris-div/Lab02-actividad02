1870e420744f463f3051ab66b23a40ca97e0760f
Cris-div <christian.unocc@tecsup.edu.pe>
2026-03-24 21:33:53 -0500

    modificaciones del diseño

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use(express.static(__dirname));
+
 // CONEXIÓN A POSTGRESQL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
